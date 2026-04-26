a42b50dcf57fff4488f312df2c3e04b9cb8e61e5
Chinmaya-droid0 <chinmayacodes@yahoo.com>
2026-04-26 21:55:39 +0530

    updated script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+let draggedCard  = null;
+
 function addTask(id){
     const input = document.getElementById(`${id}-input`);
-    const taskText = input.value;
+    const taskText = input.value.trim();
     console.log(taskText);
 
-    if(taskText === " "){
+    if(taskText === ""){
         return;
     }
     
@@ -15,7 +17,31 @@ input.value = " ";
 }
 
 function createTaskElement(taskText){
-    const taskElement= document.createElement("div");
-    taskElement.textContent = taskText;
-    return taskElement;
+    const taskElement2 = document.createElement("div");
+    taskElement2.textContent = taskText;
+    taskElement2.classList.add("task");
+    taskElement2.setAttribute("draggable", true);
+    taskElement2.addEventListener("dragstart",  dragStart);
+    taskElement2.addEventListener("dragend", dragEnd);
+    return taskElement2;
 }
+
+function dragStart(){
+    this.classList.add("dragging");
+    draggedCard = this;
+}
+
+function dragEnd(){
+    this.classList.remove("dragging");
+}
+
+const columns = document.querySelectorAll(".column  .tasks");
+ columns.forEach((column)=>{
+    column.addEventListener("dragover", dragOver);
+ }); 
+
+ function dragOver(event){
+    event.preventDefault();
+    this.appendChild(draggedCard);
+ }
+
