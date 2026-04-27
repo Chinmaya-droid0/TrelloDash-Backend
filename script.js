8142294a133e501420613f6e31401215c7f27346
Chinmaya-droid0 <chinmayacodes@yahoo.com>
2026-04-27 13:09:07 +0530

    updated

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let draggedCard  = null;
-
+//let draggedCard  = null;
+ let rightClickedCard = null;
 function addTask(id){
     const input = document.getElementById(`${id}-input`);
     const taskText = input.value.trim();
@@ -8,27 +8,33 @@ function addTask(id){
     if(taskText === ""){
         return;
     }
-    
-const taskElement = createTaskElement(taskText);
+
+const taskDate = new Date().toLocaleString();
+const taskElement = createTaskElement(taskText, taskDate);
 
 document.getElementById(`${id}-tasks`).appendChild(taskElement);  
 
 input.value = " ";
 }
 
-function createTaskElement(taskText){
+function createTaskElement(taskText, taskDate){
     const taskElement2 = document.createElement("div");
-    taskElement2.textContent = taskText;
+    taskElement2.innerHTML= `<span>${taskText}</span><br><small class="time">${taskDate}</small>`;
     taskElement2.classList.add("task");
     taskElement2.setAttribute("draggable", true);
     taskElement2.addEventListener("dragstart",  dragStart);
     taskElement2.addEventListener("dragend", dragEnd);
+    taskElement2.addEventListener("contextmenu", function(event) {
+        event.preventDefault();
+        rightClickedCard = this;
+        showContextMenu(event.pageX, event.pageY);
+    })
     return taskElement2;
 }
 
 function dragStart(){
     this.classList.add("dragging");
-    draggedCard = this;
+    //draggedCard = this;
 }
 
 function dragEnd(){
@@ -39,9 +45,42 @@ const columns = document.querySelectorAll(".column  .tasks");
  columns.forEach((column)=>{
     column.addEventListener("dragover", dragOver);
  }); 
-
+ 
  function dragOver(event){
     event.preventDefault();
-    this.appendChild(draggedCard);
- }
+    const draggedCard = document.querySelector(".dragging");
+    this.appendChild(draggedCard); 
+ } 
 
+ const contextmenu = document.querySelector(".context-menu");
+function showContextMenu(x, y) {
+    contextmenu.style.left = `${x}px`;
+    contextmenu.style.top = `${y}px`;
+    contextmenu.style.display= "block" ;
+}
+
+document.addEventListener("click", ()=>{
+    contextmenu.style.display= "none";
+})
+
+function editTask(){
+     if(rightClickedCard != null){
+        const newTaskText = prompt("Edit Task -", rightClickedCard.textContent);
+
+    if(newTaskText !== ""){
+        rightClickedCard.textContent = newTaskText;
+    }
+     }
+     
+}
+
+function deleteTask() {
+    if(rightClickedCard !== null){
+        rightClickedCard.remove();
+    }
+}
+
+function upadteTasksCount(id) {
+    const count = document.querySelectorAll(`#${id}-tasks .tasks`).length;
+    document.getElementById(`${id}-count`).textContent = count;
+}
